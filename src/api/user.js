bf98e45388a53c379926f8717ba6c6e17b152a86
Reyhan <r.rahmanipour@gmail.com>
2020-07-05 21:47:08 +0430

    yarn pacage added, signin screen created but not work correctly yet

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { AsyncStorage } from 'react-native';
+import { AsyncStorage } from '@react-native-community/async-storage';
 
 const instance = axios.create({
   baseURL: 'https://neocardbackend.herokuapp.com/api'
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SigninScreen = () => {
-    return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
-}
+  const { state, signin, clearErrorMessage } = useContext(AuthContext);
 
-const styles = StyleSheet.create({});
+  return (
+    <View style={styles.container}>
+      <NavigationEvents onWillBlur = { clearErrorMessage } />
+      <AuthForm 
+        headaeText= "Sign in to your account"
+        errorMessage= {state.errorMessage}
+        submitButtonText= "Sign In"
+        onSubmit= {signin}
+      />
+      <NavLink 
+        routeName= "Signup"
+        text= "Don't have an account? Sign up instead"
+      />
+    </View>
+  );
+};
+
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250
+  }
+});
 
 export default SigninScreen;
