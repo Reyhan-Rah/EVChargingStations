ced03500aed7f5f423fdac1a76ee73fb60d5f5eb
Reyhan <r.rahmanipour@gmail.com>
2020-08-13 18:44:06 +0430

    companyScreen showing list of companies successfully

--- App.js
+++ App.js
@@ -4,18 +4,21 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import { Provider as AuthProvider } from './src/context/AuthContext';
 import { Provider as StationsProvider } from './src/context/StationsContext';
+import { Provider as CompanyProvider } from './src/context/CompanyContext';
 import { navigationRef } from './src/navigationRef';
 import Routers from './src/Routers';
 
 const App = () => {
   return (
-  <StationsProvider>
-    <AuthProvider>
-     <NavigationContainer ref={ navigationRef }>
-        <Routers />
-      </NavigationContainer>
-    </AuthProvider>
-   </StationsProvider>
+    <CompanyProvider>
+      <StationsProvider>
+        <AuthProvider>
+        <NavigationContainer ref={ navigationRef }>
+            <Routers />
+          </NavigationContainer>
+        </AuthProvider>
+      </StationsProvider>
+    </CompanyProvider>
   );
 };
 
--- src/screens/CompanyScreen.js
+++ src/screens/CompanyScreen.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext, useEffect } from 'react';
+import { Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { Context as CompanyContext } from '../context/CompanyContext';
+import { ListItem } from 'react-native-elements';
 
 const CompanyScreen = () => {
-    return (
-    <Text style={{ fontSize: 48 }}>CompanyScreen</Text>
-    );
-}
+const { state, fetchCompanies } = useContext(CompanyContext);
+
+  useEffect(() => {
+    fetchCompanies();
+  }, []);
+
+  return (
+    <>
+      <Text style={{ fontSize: 30 }}>Companies List</Text>
+      <FlatList
+        data={state}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity>
+              <ListItem chevron title={item.name} />
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </>
+  );
+};
 
 const styles = StyleSheet.create({});
 
